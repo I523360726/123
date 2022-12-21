4c7b2b744494d16ff94705443aac58271154f29b
I523360726 <121203917+I523360726@users.noreply.github.com>
2022-12-22 07:56:32 +0800

    Update cbb.f

--- cbb/cbb.f
+++ cbb/cbb.f
@@ -1,16 +1,16 @@
--v $PRJ_ROOT/cbb/crc/crc8_serial.sv
--v $PRJ_ROOT/cbb/crc/crc8to8_parallel.sv
--v $PRJ_ROOT/cbb/crc/crc16to8_parallel.sv
+-v $PRJ_ROOT/cbb/crc8_serial.sv
+-v $PRJ_ROOT/cbb/crc8to8_parallel.sv
+-v $PRJ_ROOT/cbb/crc16to8_parallel.sv
 
--v $PRJ_ROOT/cbb/misc/gnrl_sync.sv
--v $PRJ_ROOT/cbb/misc/rstn_merge.sv
--v $PRJ_ROOT/cbb/misc/rstn_sync.sv
--v $PRJ_ROOT/cbb/misc/signal_detect.sv
--v $PRJ_ROOT/cbb/misc/signal_extend.sv
--v $PRJ_ROOT/cbb/misc/tree_adder.sv
+-v $PRJ_ROOT/cbb/gnrl_sync.sv
+-v $PRJ_ROOT/cbb/rstn_merge.sv
+-v $PRJ_ROOT/cbb/rstn_sync.sv
+-v $PRJ_ROOT/cbb/signal_detect.sv
+-v $PRJ_ROOT/cbb/signal_extend.sv
+-v $PRJ_ROOT/cbb/tree_adder.sv
 
--v $PRJ_ROOT/cbb/reg/ro_reg.sv
--v $PRJ_ROOT/cbb/reg/rw_reg.sv
--v $PRJ_ROOT/cbb/reg/rwc_reg.sv
--v $PRJ_ROOT/cbb/reg/rww_reg.sv
--v $PRJ_ROOT/cbb/reg/wo_reg.sv
+-v $PRJ_ROOT/cbb/ro_reg.sv
+-v $PRJ_ROOT/cbb/rw_reg.sv
+-v $PRJ_ROOT/cbb/rwc_reg.sv
+-v $PRJ_ROOT/cbb/rww_reg.sv
+-v $PRJ_ROOT/cbb/wo_reg.sv
