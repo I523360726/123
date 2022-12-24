07dcd09a606340d000dc924fab03012d67ff3238
I523360726 <121203917+I523360726@users.noreply.github.com>
2022-12-24 12:32:05 +0800

    Update lv_lbist.sv

--- lv_top/lv_lbist.sv
+++ lv_top/lv_lbist.sv
@@ -12,17 +12,19 @@ module lv_lbist #(
     `include "lv_param.svh"
     parameter END_OF_LIST          = 1
 )( 
-    input  logic 		   i_bist_en                ,
+    input  logic           i_bist_en                ,
 
     output logic           o_bist_wdg_owt_tx_req    ,
     input  logic           i_owt_rx_ack             ,
     input  logic           i_owt_rx_status          ,
+    output logic           o_owt_bist_rutl          ,
 
     output logic           o_bist_scan_reg_req      ,
     input  logic           i_scan_reg_bist_ack      ,
     input  logic           i_scan_reg_bist_err      ,
+    output logic           o_scan_reg_bist_rult     ,
 
-    output logic           o_lv_bist_fail           ,
+    output logic           o_lv_bist_done           ,
 
     input  logic           i_clk                    ,
     input  logic           i_rst_n
@@ -34,7 +36,7 @@ localparam SCAN_CNT_W           = $clog2(LV_SCAN_REG_NUM+1) ;
 localparam BIST_OWT_TX_NUM      = 4                         ;
 localparam BIST_OWT_TX_CNT_W    = $clog2(BIST_OWT_TX_NUM+1) ;
 localparam BIST_OWT_TX_OK_NUM   = 3                         ;
-localparam BIST_TMO_TH          = 20000*CLK_M               ;
+localparam BIST_TMO_TH          = 2000*CLK_M                ;//2000*48 cycle.
 localparam BIST_TMO_CNT_W       = $clog2(BIST_TMO_TH)       ;
 //==================================
 //var delcaration
@@ -44,87 +46,85 @@ logic                           scan_reg_bist_err   ;
 logic [BIST_OWT_TX_CNT_W-1: 0]  owt_tx_cnt          ;
 logic [BIST_OWT_TX_CNT_W-1: 0]  owt_rx_ok_cnt       ;
 logic [BIST_TMO_CNT_W-1:    0]  bist_tmo_cnt        ;
+logic                           owt_bist_fail       ;
 //==================================
 //main code
 //==================================
 //scan reg
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
-	    scan_cnt <= SCAN_CNT_W'(0);
-	end
+        scan_cnt <= SCAN_CNT_W'(0);
+    end
     else if(i_bist_en) begin
-  	    if(i_scan_reg_bist_ack) begin
-	        scan_cnt <= (scan_cnt==LV_SCAN_REG_NUM) ? scan_cnt : (scan_cnt+1'b1);
-	    end
-        else;
-    end
-    else begin
-	    scan_cnt <= SCAN_CNT_W'(0);    
-    end
-end
-
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-	    o_bist_scan_reg_req <= 1'b0;
-	end
-    else if(i_scan_reg_bist_ack) begin
-	    o_bist_scan_reg_req <= 1'b0;    
-    end
-  	else if(i_bist_en & (scan_cnt<LV_SCAN_REG_NUM)) begin
-	    o_bist_scan_reg_req <= 1'b1;
-	end
-    else begin
-	    o_bist_scan_reg_req <= 1'b0;
-    end
-end
-
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-	    scan_reg_bist_err <= 1'b0;
-	end
-    else if(i_bist_en) begin
-        if(i_scan_reg_bist_ack & i_scan_reg_bist_err) begin
-            scan_reg_bist_err <= 1'b1;
+        if(i_scan_reg_bist_ack) begin
+            scan_cnt <= (scan_cnt==LV_SCAN_REG_NUM) ? scan_cnt : (scan_cnt+1'b1);
         end
         else;
     end
     else begin
-	    scan_reg_bist_err <= 1'b0;
+        can_cnt <= SCAN_CNT_W'(0);    
     end
 end
+
+always_ff@(posedge i_clk or negedge i_rst_n) begin
+    if(~i_rst_n) begin
+        o_bist_scan_reg_req <= 1'b0;
+    end
+    else if(i_scan_reg_bist_ack) begin
+        o_bist_scan_reg_req <= 1'b0;    
+    end
+    else if(i_bist_en & (scan_cnt<LV_SCAN_REG_NUM)) begin
+        o_bist_scan_reg_req <= 1'b1;
+    end
+    else begin
+        o_bist_scan_reg_req <= 1'b0;
+    end
+end
+
+always_ff@(posedge i_clk or negedge i_rst_n) begin
+    if(~i_rst_n) begin
+        scan_reg_bist_err <= 1'b0;
+    end
+    else if(i_bist_en & i_scan_reg_bist_ack & i_scan_reg_bist_err) begin
+        scan_reg_bist_err <= 1'b1;
+    end
+    else;
+end
+
+assign o_scan_reg_bist_rult = ~scan_reg_bist_err;
 
 //owt tx
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
-	    owt_tx_cnt <= BIST_OWT_TX_CNT_W'(0);
-	end
-  	else if(i_owt_rx_ack) begin
-	    owt_tx_cnt <= (owt_tx_cnt==BIST_OWT_TX_NUM) ? owt_tx_cnt : (owt_tx_cnt+1'b1);
-	end
-    else begin
-	    owt_tx_cnt <= BIST_OWT_TX_CNT_W'(0);    
+        owt_tx_cnt <= BIST_OWT_TX_CNT_W'(0);
     end
-end
-
-always_ff@(posedge i_clk or negedge i_rst_n) begin
-    if(~i_rst_n) begin
-	    o_bist_wdg_owt_tx_req <= 1'b0;
-	end
     else if(i_owt_rx_ack) begin
-	    o_bist_wdg_owt_tx_req <= 1'b0;    
+        owt_tx_cnt <= (owt_tx_cnt==BIST_OWT_TX_NUM) ? owt_tx_cnt : (owt_tx_cnt+1'b1);
     end
-  	else if(i_bist_en & (owt_tx_cnt<BIST_OWT_TX_NUM)) begin
-	    o_bist_wdg_owt_tx_req <= 1'b1;
-	end
     else begin
-	    o_bist_wdg_owt_tx_req <= 1'b0;
+        owt_tx_cnt <= BIST_OWT_TX_CNT_W'(0);    
     end
 end
 
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
-	    owt_rx_ok_cnt <= BIST_OWT_TX_CNT_W'(0);
-	end
+        o_bist_wdg_owt_tx_req <= 1'b0;
+    end
+    else if(i_owt_rx_ack) begin
+        o_bist_wdg_owt_tx_req <= 1'b0;    
+    end
+    else if(i_bist_en & (owt_tx_cnt<BIST_OWT_TX_NUM)) begin
+        o_bist_wdg_owt_tx_req <= 1'b1;
+    end
+    else begin
+        o_bist_wdg_owt_tx_req <= 1'b0;
+    end
+end
+
+always_ff@(posedge i_clk or negedge i_rst_n) begin
+    if(~i_rst_n) begin
+        owt_rx_ok_cnt <= BIST_OWT_TX_CNT_W'(0);
+    end
     else if(i_bist_en) begin
         if(i_owt_rx_ack & ~i_owt_rx_status) begin
             owt_rx_ok_cnt <= owt_rx_ok_cnt+1'b1;
@@ -132,38 +132,54 @@ always_ff@(posedge i_clk or negedge i_rst_n) begin
         else;
     end
     else begin
-	    owt_rx_ok_cnt <= BIST_OWT_TX_CNT_W'(0);
+        owt_rx_ok_cnt <= BIST_OWT_TX_CNT_W'(0);
     end
 end
 
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
-	    bist_tmo_cnt <= BIST_TMO_CNT_W'(0);
-	end
-  	else if(i_bist_en) begin
-	    bist_tmo_cnt <= (bist_tmo_cnt==(BIST_TMO_TH-1)) ? bist_tmo_cnt : (bist_tmo_cnt+1'b1);
-	end
+        bist_tmo_cnt <= BIST_TMO_CNT_W'(0);
+    end
+    else if(i_bist_en) begin
+        bist_tmo_cnt <= (bist_tmo_cnt==(BIST_TMO_TH-1)) ? bist_tmo_cnt : (bist_tmo_cnt+1'b1);
+    end
     else begin
-	    bist_tmo_cnt <= BIST_TMO_CNT_W'(0);    
+        bist_tmo_cnt <= BIST_TMO_CNT_W'(0);    
     end
 end
 
 always_ff@(posedge i_clk or negedge i_rst_n) begin
     if(~i_rst_n) begin
-	    o_lv_bist_fail <= 1'b0;
-	end
-  	else if(i_bist_en & (bist_tmo_cnt==(BIST_TMO_TH-1))) begin
-        if((owt_rx_ok_cnt<BIST_OWT_TX_OK_NUM) | scan_reg_bist_err) begin
-	        o_lv_bist_fail <= 1'b1;
+        owt_bist_fail <= 1'b0;
+    end
+    else if(i_bist_en & (bist_tmo_cnt==(BIST_TMO_TH-1))) begin
+        if(owt_rx_ok_cnt<BIST_OWT_TX_OK_NUM) begin
+            owt_bist_fail <= 1'b1;
         end
         else begin
-	        o_lv_bist_fail <= 1'b0;            
+            owt_bist_fail <= 1'b0;            
         end
-	end
+    end
+    else;
+end
+
+assign o_owt_bist_rutl = ~owt_bist_fail;
+
+always_ff@(posedge i_clk or negedge i_rst_n) begin
+    if(~i_rst_n) begin
+        o_lv_bist_done <= 1'b0;
+    end
+    else if(i_bist_en) begin
+        if(bist_tmo_cnt==(BIST_TMO_TH-1)) begin
+            o_lv_bist_done <= 1'b1;
+        end
+        else;
+    end
     else begin
-        o_lv_bist_fail <= 1'b0;            
+        o_lv_bist_done <= 1'b0;            
     end
 end
+
 
 // synopsys translate_off    
 //==================================
@@ -172,4 +188,5 @@ end
 //    
 // synopsys translate_on    
 endmodule
+
 
