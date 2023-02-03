a7497242ff1fd03952aea78e4b59e9b422233fa6
I523360726 <121203917+I523360726@users.noreply.github.com>
2023-02-03 09:42:28 +0800

    Create symbol_measure.sv

---
+++ com_mdl/symbol_measure.sv
@@ -0,0 +1,18 @@
+//============================================================
+//Module   : symbol measure
+//Function : measure symbol len
+//File Tree: 
+//-------------------------------------------------------------
+//Update History
+//-------------------------------------------------------------
+//Rev.level     Date          Code_by         Contents
+//1.0           2022/11/6     xxxx            Create
+//=============================================================
+module symbol_measure #(
+    `include "com_param.svh"
+    parameter END_OF_LIST = 1
+)(
+
+);
+  
+endmodule
